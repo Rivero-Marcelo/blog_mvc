a6c2c9c9609d7d140476dbf6a525a44fd10d252a
Marcelo Rivero <rivero_marcelo@hotmail.com>
2022-07-04 23:25:54 -0300

    Hechas consultas sql para crear tablas

--- db.sql
+++ db.sql
@@ -1,6 +1,27 @@
 CREATE TABLE usuario(
     id int primary key auto_increment,
     username varchar(50) unique,
-    password varchar(255)
+    password varchar(255),
+    nombre varchar(25),
+    apellido varchar(25)
 );
 
+
+
+CREATE TABLE `blog`.`publicacion` (
+  `idPublicacion` INT NOT NULL AUTO_INCREMENT,
+  `idAutor` INT NOT NULL,
+  `fechaHora` DATETIME NOT NULL,
+  `cuerpo` VARCHAR(2500) NOT NULL,
+  PRIMARY KEY (`idPublicacion`));
+
+
+  ALTER TABLE `blog`.`publicacion` 
+ADD INDEX `idAutor_idx` (`idAutor` ASC);
+;
+ALTER TABLE `blog`.`publicacion` 
+ADD CONSTRAINT `idAutor`
+  FOREIGN KEY (`idAutor`)
+  REFERENCES `blog`.`usuario` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
